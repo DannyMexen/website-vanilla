d88671f89bc48b85b3edad67f6e2397cca7cafe0
Daniel M. Mwale <danny@arcariusmexen.com>
2023-03-23 15:46:48 +0200

    uniform names for headers

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,16 @@
 // Author: Danny Mexen
 // Date: Tue 23 Mar 2023
 
-// Company name
-const amws = "AMWS";
-const amwsFull = `Arcarius Mexen Web Services (${amws})`;
 
 // <head>
 const title = document.querySelector('title');
-title.textContent = amws;
+title.textContent = "AMWS";
+
 
 // <header>
 const companyName = document.querySelector('#company-name');
-companyName.textContent = amwsFull;
+companyName.textContent = "Arcarius Mexen Web Services (AMWS)";
 const motto = document.querySelector("#motto");
 motto.textContent = "Simple, Elegant.";
 const tagline = document.querySelector("#tagline");
-tagline.textContent = "Creative trustee for all your enterprise applications.";
+tagline.textContent = "Creative trustee for all your enterprise application needs.";
