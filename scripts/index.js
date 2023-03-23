c27a54cdd35b07675606e42a6eb9fdb829edbc0a
Daniel M. Mwale <danny@arcariusmexen.com>
2023-03-23 15:51:36 +0200

    single ladies

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,17 @@
 
 // <head>
 const title = document.querySelector('title');
-title.textContent = "AMWS";
+title.textContent = 'AMWS';
+// TODO: meta
 
 
 // <header>
 const companyName = document.querySelector('#company-name');
-companyName.textContent = "Arcarius Mexen Web Services (AMWS)";
-const motto = document.querySelector("#motto");
-motto.textContent = "Simple, Elegant.";
-const tagline = document.querySelector("#tagline");
-tagline.textContent = "Creative trustee for all your enterprise application needs.";
+companyName.textContent = 'Arcarius Mexen Web Services (AMWS)';
+const motto = document.querySelector('#motto');
+motto.textContent = 'Simple, Elegant.';
+const tagline = document.querySelector('#tagline');
+tagline.textContent = 'Creative trustee for all your enterprise application needs.';
+
+// <nav>
+const services = document.querySelector('#services');
